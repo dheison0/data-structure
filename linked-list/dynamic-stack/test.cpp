d5cd8229f3af58770e122b9554f6ad860c413675
Dheison Gomes <dheisomgomes0@gmail.com>
2023-09-21 20:23:29 -0300

    Fix possible crash on system when running stress test o dynamic stack

--- linked-list/dynamic-stack/test.cpp
+++ linked-list/dynamic-stack/test.cpp
@@ -8,10 +8,10 @@ int main() {
   DynamicStack stack;
   string d;
   while (1) {
-    for (int i = 1; i <= 999999999; i++) {
+    for (int i = 1; i <= 1000000; i++) {
       stack.insert(TypeItem(i * i));
     }
-    cout << "Press enter to add more 999999999 items";
+    cout << "Press enter to add more 1000000 items";
     cin >> d;
   }
   return 0;
